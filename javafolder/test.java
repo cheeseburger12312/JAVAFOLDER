cfc8d2838cc81f7ba6b0905876db1fbc38679fd4
Syntax <ictcompetencestudentno5@gmail.com>
2026-06-17 13:15:04 +0800

    test

---
+++ javafolder/test.java
@@ -0,0 +1,11 @@
+package javafolder;
+
+public class test {
+    public static void main(String[] argument) {
+        String name = "Aramis";
+        String surname = "Gayon";
+        System.out.printf("my name is %s%n",name);
+        System.out.printf("my name is %s%n",surname);
+    }
+}
+ 
