7ea796f10e0d5efe20032cb3fc3504913ca9217a
Syntax <ictcompetencestudentno5@gmail.com>
2026-06-17 14:18:08 +0800

    123

--- javafolder/test.java
+++ javafolder/test.java
@@ -6,6 +6,19 @@ public class test {
         String surname = "Gayon";
         System.out.printf("my name is %s%n",name);
         System.out.printf("my name is %s%n",surname);
+
+        System.out.println("Addition");
+        System.out.println("5 + 7 = " + (5 + 7));
+
+        System.out.println("Subtraction");
+        System.out.println("5 - 3 = " + (5 - 3));
+
+        System.out.println("Multiplication");
+        System.out.println("10 * 3 = " + (10 * 3));
+
+        System.out.println("devision");
+        System.out.println("2 / 6 = " + (10 / 6));
+
     }
 }
  
